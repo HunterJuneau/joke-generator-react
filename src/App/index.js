b68b76d8389f3dc38efeca148dce319b71ebc135
HunterJuneau <huntlee17@gmail.com>
2021-04-20 21:43:47 -0500

    Created joke generator.

--- src/App/index.js
+++ src/App/index.js
@@ -1,13 +1,42 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Jumbotron, Button } from 'reactstrap';
 import './App.scss';
+import getRandomJoke from '../jokeData';
 
-function App() {
+const App = () => {
+  const [joke, setJoke] = useState({});
+  const [punchline, setPunchline] = useState(true);
+
+  const clickHandler = () => {
+    if (punchline) {
+      getRandomJoke().then((jokeObject) => setJoke(jokeObject));
+      setPunchline(false);
+    } else {
+      setPunchline(true);
+    }
+  };
+
   return (
     <div className='App'>
-      <h2>INSIDE APP COMPONENT</h2>
-      <button className='btn btn-info'>I am a button</button>
+      <Jumbotron>
+        <h1 className='display-3'>Joke Generator</h1>
+        <hr className='my-2' />
+        <p className='lead'>
+          <Button onClick={clickHandler} color='primary'>
+            {punchline ? 'New Joke' : 'Tell Me!'}
+          </Button>
+        </p>
+        {joke ? (
+          <>
+            <h5>{joke.setup}</h5>
+            {punchline ? <h6>{joke.punchline}</h6> : ''}
+          </>
+        ) : (
+          ''
+        )}
+      </Jumbotron>
     </div>
   );
-}
+};
 
 export default App;
